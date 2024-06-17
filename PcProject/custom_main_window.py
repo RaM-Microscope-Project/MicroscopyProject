ea8471e5c5c60fe653ccad887431879f94e67d3e
MateiObrocea <matei.obrocea@gmail.com>
2024-06-17 18:03:40 +0200

    Configured all sliders

--- PcProject/custom_main_window.py
+++ PcProject/custom_main_window.py
@@ -2,14 +2,28 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtCore, QtWidgets
 
 from PcProject.graphical_user_interface import Ui_MainWindow
+from custom_slider import CustomSlider
 
 
 class CustomMainWindow(QMainWindow):
     def __init__(self):
+        """
+        Constructor for the custom main window.
+        Self.ui is essentially the main window.
+        All the elements of the main window are accessible through self.ui.
+        Examples: self.ui.whiteBalanceSlider, self.ui.pushButton, etc.
+        """
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # ------- define the sliders -------
+        self.whiteSlider = CustomSlider(self.ui.whiteBalanceSlider, 0, 400, self.ui.whiteBalanceValueLabel)
+        self.analogSlider = CustomSlider(self.ui.analogGainSlider, 0, 100, self.ui.analogGainValueLabel)
+        self.contrastSlider = CustomSlider(self.ui.contrastSlider, 0, 100, self.ui.contrastValueLabel)
+        self.sharpnessSlider = CustomSlider(self.ui.sharpnessSlider, 0, 100, self.ui.sharpnessValueLabel)
+        self.saturationSlider = CustomSlider(self.ui.saturationSlider, 0, 100, self.ui.saturationValueLabel)
+        self.brightnessSlider = CustomSlider(self.ui.brightnessSlider, 0, 100, self.ui.brightnessValueLabel)
 
 import resources_rc
 
---
+++ PcProject/custom_slider.py
@@ -0,0 +1,19 @@
+class CustomSlider:
+    def __init__(self, slider, minimum, maximum, label):
+        self.slider = slider
+        self.slider.setMinimum(minimum)
+        self.slider.setMaximum(maximum)
+        self.label = label
+        self.slider.valueChanged.connect(self.update_label)
+        self.set_slider_to_middle()
+
+
+    def set_slider_to_middle(self):
+        middle_value = (self.slider.minimum() + self.slider.maximum()) // 2
+        self.slider.setValue(middle_value)
+
+    def update_label(self):
+        self.label.setText(str(self.slider.value()/100))
+
+    def print_slider_value(self):
+        print(self.slider.value()/100)
