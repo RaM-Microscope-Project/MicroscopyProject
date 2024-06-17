b23be9f3aaba019e64971d8642d0c2089b5f562a
OPSD2k <98692454+OPSD2k@users.noreply.github.com>
2024-06-17 16:29:45 +0200

    Added LED control

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,132 +1,192 @@
-/* 
-  Example sketch to control a 28BYJ-48 stepper motor 
-  with ULN2003 driver board and Arduino UNO. 
-  More info: https://www.makerguides.com 
-*/
-
-#include "Stepper.h"
 #include <AccelStepper.h>
+#include <FastLED.h>
 
-// Define number of steps per rotation:
-const int angular_units = 2048;
+// LED strip configuration
+#define LED_PIN 14
+#define NUM_LEDS 6
+#define BRIGHTNESS 255
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
-// Wiring:
-// Pin 8 to IN1 on the ULN2003 driver
-// Pin 9 to IN2 on the ULN2003 driver
-// Pin 10 to IN3 on the ULN2003 driver
-// Pin 11 to IN4 on the ULN2003 driver
+CRGB leds[NUM_LEDS];
 
-// Pin 4 to IN1 on the ULN2003 driver
-// Pin 5 to IN2 on the ULN2003 driver
-// Pin 6 to IN3 on the ULN2003 driver
-// Pin 7 to IN4 on the ULN2003 driver
+// Define number of steps per revolution for the stepper motor
+const int stepsPerRevolution = 2048;
 
-// Pin 2 to IN1 on the ULN2003 driver
-// Pin 3 to IN2 on the ULN2003 driver
-// Pin 12 to IN3 on the ULN2003 driver
-// Pin 13 to IN4 on the ULN2003 driver
+//Motor layout when looking at front end of microscope
+//1 (A); Top left
+//2 (B); Top right
+//3 (C); Bottom
 
-// Create stepper object called 'myStepper', note the pin order:
-//pins are such that wires can be inserted corresponding to driver board pins
-Stepper stepper1 = Stepper(2048, 8, 10, 9, 11); //The first argument is the resolution of the stepper motor
-Stepper stepper2 = Stepper(2048, 4, 6, 5, 7); //The first argument is the resolution of the stepper motor
-Stepper stepper3 = Stepper(2048, 3, 12, 2, 13); //9
+// Initialize the first stepper motor on pins 8, 10, 9, 11
+AccelStepper stepper1(AccelStepper::FULL4WIRE, 8, 10, 9, 11);
 
-//AccelStepper stepper1(AccelStepper::FULL4WIRE, 8, 10, 9, 11);
-//AccelStepper stepper2(AccelStepper::FULL4WIRE, 4, 6, 5, 7);
-//AccelStepper stepper3(AccelStepper::FULL4WIRE, 3, 12, 2, 13);
+// Initialize the second stepper motor on pins 4, 6, 5, 7
+AccelStepper stepper2(AccelStepper::FULL4WIRE, 4, 6, 5, 7);
+
+// Initialize the third stepper motor on pins 2, 13, 3, 12
+AccelStepper stepper3(AccelStepper::FULL4WIRE, 2, 13, 3, 12);
+
+long targetPos1 = 0;
+long targetPos2 = 0;
+long targetPos3 = 0;
 
 void setup() {
-  // Set the speed to 5 rpm:
-  stepper1.setSpeed(15);
-  stepper2.setSpeed(15);
-  stepper3.setSpeed(15);
+  // LED setup
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  // Turn off all LEDs at the start
+  FastLED.clear();
+  FastLED.show();
+
   Serial.begin(9600);
+
+  // Set maximum speed and acceleration for each motor
+
+  stepper1.setMaxSpeed(1000);
+  stepper1.setAcceleration(5000);
+
+  stepper2.setMaxSpeed(1000);
+  stepper2.setAcceleration(5000);
+
+  stepper3.setMaxSpeed(1000);
+  stepper3.setAcceleration(5000);
+
+  //debug test if motors run at all
+  //stepper1.setSpeed(500);
+  //stepper2.setSpeed(500);
+  //stepper3.setSpeed(500);
+
+  //digitalWrite(A0, HIGH);
 }
 
 void loop() {
-  Serial.println("clockwise"); //out
-  stepper1.step(-angular_units * 3); //how many steps to move
+  // Check for serial input
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+    int ledIndex = 0;
 
-  Serial.println("counterclockclockwise"); //in
-  stepper1.step(angular_units * 3);
+    //Toggle corresponding LED based on key press
+    if (input >= '1' && input <= '6') {
+      Serial.print("key is ");
+      Serial.println(input);
+      ledIndex = input - '1';
+      //toggleLED(ledIndex, CRGB(255, 255, 50));  //pure white
+      toggleLED(ledIndex, CRGB(255, 255, 20));  //warm white
+    }
 
-  Serial.println("clockwise"); //out
-  stepper2.step(-angular_units * 3); //how many steps to move
+    if (input == 'g'){
+      Serial.print("key is ");
+      Serial.println(input);
+      toggleLED(ledIndex, CRGB(255, 255, 20));  //warm white
+    }
 
-  Serial.println("counterclockclockwise"); //in
-  stepper2.step(angular_units * 3);
+    if (input == 'h'){
+      Serial.print("key is ");
+      Serial.println(input);
+      toggleLED(ledIndex, CRGB(255, 255, 20));  //warm white
+    }
 
-  Serial.println("clockwise"); //out
-  stepper3.step(-angular_units * 3); //how many steps to move
+    if (input == 'd') {
+      Serial.println("Key D pressed. Moving motors...");
+      // Move stepper1 clockwise and stepper2 counterclockwise
+      stepper1.setSpeed(500);
+      stepper2.setSpeed(-500);
+      stepper3.setSpeed(0);
+    }
 
-  Serial.println("counterclockclockwise"); //in
-  stepper3.step(angular_units * 3);
+    else if (input == 'a') {
+      Serial.println("Key A pressed. Moving motors...");
+      // Move stepper1 counterclockwise and stepper2 clockwise
+      stepper1.setSpeed(-500);
+      stepper2.setSpeed(500);
+      stepper3.setSpeed(0);
+    }
+
+    else if (input == 's') {
+      Serial.println("Key S pressed. Moving motors...");
+      // Move stepper1 and stepper2 clockwise, stepper3 counterclockwise
+      stepper1.setSpeed(500);
+      stepper2.setSpeed(500);
+      stepper3.setSpeed(-500);
+    }
+
+    else if (input == 'w') {
+      Serial.println("Key W pressed. Moving motors...");
+      // Move stepper1 and stepper2 counterclockwise, stepper3 clockwise
+      stepper1.setSpeed(-500);
+      stepper2.setSpeed(-500);
+      stepper3.setSpeed(500);
+    }
+
+    else if (input == 'q') {
+      Serial.println("Q for Quit. Stopping all motors...");
+      // Move stepper1 and stepper2 counterclockwise, stepper3 clockwise
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(0);
+      stepper3.setSpeed(0);
+    }
+
+    else if (input == 'i') {
+      Serial.println("Motor 1 running...");
+      // Move stepper1 anticlockwise
+      stepper1.setSpeed(500);
+      stepper2.setSpeed(0);
+      stepper3.setSpeed(0);
+    }
+
+    else if (input == 'j') {
+      Serial.println("Motor 1 running...");
+      // Move stepper1 clockwise
+      stepper1.setSpeed(-500);
+      stepper2.setSpeed(0);
+      stepper3.setSpeed(0);
+    }
+
+    else if (input == 'o') {
+      Serial.println("Motor 2 running...");
+      // Move stepper2 anticlockwise
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(500);
+      stepper3.setSpeed(0);
+    }
+
+    else if (input == 'k') {
+      Serial.println("Motor 2 running...");
+      // Move stepper2 clockwise
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(-500);
+      stepper3.setSpeed(0);
+    }
+
+    else if (input == 'p') {
+      Serial.println("Motor 3 running...");
+      // Move stepper3 anticlockwise
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(0);
+      stepper3.setSpeed(500);
+    }
+
+    else if (input == 'l') {
+      Serial.println("Motor 3 running...");
+      // Move stepper3 anticlockwise
+      stepper1.setSpeed(0);
+      stepper2.setSpeed(0);
+      stepper3.setSpeed(-500);
+    }
+  }
+
+  stepper1.runSpeed();
+  stepper2.runSpeed();
+  stepper3.runSpeed();
 }
 
-/* new, non-functional
-void setup() {
-  // Set the speed to 15 RPM (steps per second is speed multiplied by steps per revolution divided by 60)
-  stepper1.setMaxSpeed(100);
-  stepper2.setMaxSpeed(100);
-  stepper3.setMaxSpeed(100);
-  
-  Serial.begin(9600);
+void toggleLED(int ledIndex, CRGB color) {
+  if (leds[ledIndex] == CRGB::Black) {
+    leds[ledIndex] = color;  // Set LED to warm white
+  } else {
+    leds[ledIndex] = CRGB::Black;  // Turn off LED
+  }
+  FastLED.show();
 }
-
-void loop() {
-  // Run stepper1
-  Serial.println("clockwise"); // out
-  stepper1.moveTo(-angular_units * 3);
-  while (stepper1.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
-
-  Serial.println("counterclockwise"); // in
-  stepper1.moveTo(angular_units * 3);
-  while (stepper1.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
-
-  // Run stepper2
-  Serial.println("clockwise"); // out
-  stepper2.moveTo(-angular_units * 3);
-  while (stepper2.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
-
-  Serial.println("counterclockwise"); // in
-  stepper2.moveTo(angular_units * 3);
-  while (stepper2.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
-
-  // Run stepper3
-  Serial.println("clockwise"); // out
-  stepper3.moveTo(-angular_units * 3);
-  while (stepper3.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
-
-  Serial.println("counterclockwise"); // in
-  stepper3.moveTo(angular_units * 3);
-  while (stepper3.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-    stepper3.run();
-  }
-}
-*/
-
-
-
