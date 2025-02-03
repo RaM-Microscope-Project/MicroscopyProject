9f7f57dbf530f98d5f0f29b7878b91fb5bd696a6
RubenKoudijs <85734075+RubenKoudijs@users.noreply.github.com>
2025-02-03 10:47:01 +0100

    added comments

--- src/camera_widget.py
+++ src/camera_widget.py
@@ -21,6 +21,8 @@ class Camera_widget(QWidget):
         self.uiWindow = parent
         self.camera_controls = camera_controls
         self.arduino = arduino
+
+        #some variables to store the active state of the automation features
         self.capturing_RTI = False
         self.capturing_SP = False
         self.capturing_FS = False
@@ -53,7 +55,7 @@ class Camera_widget(QWidget):
         self.myPreviewWindow.show()
         camera_controls.picam2.start()
 
-    
+    #capture an image
     def on_Capture_Clicked(self):
         print("capturing image")
         self.btnChildCapture.setEnabled(False)
@@ -63,15 +65,14 @@ class Camera_widget(QWidget):
         print("- Capture image:", targetPath)
         self.camera_controls.picam2.switch_mode_and_capture_file(cfg, targetPath, signal_function=self.qpicamera2.signal_done)
 
-    
+    #this function is called whenever an image capture is finished
     def capture_done(self, job):
         result = self.camera_controls.picam2.wait(job)
         self.btnChildCapture.setEnabled(True)
-        #print("- capture_done.")
-        #print(result)
+        #call the capture_set function to capture the next image in the image set right away
         self.capture_set(result)
     
-
+    #this function checks what set is being captured, send the arduino controller class commands accordingly and captures the next image in the set
     def capture_set(self, result):
         if self.capturing_RTI:
             self.arduino.serial(f'LED0{self.count}')
@@ -99,7 +100,7 @@ class Camera_widget(QWidget):
             self.focus_stack()
 
 
-
+    #initialises the RTI function by making a directory and setting the variables
     def init_RTI(self):
         self.arduino.serial(f'LED_B{self.uiWindow.ui.inputBox_RTI.value()}')
         self.timeStamp = time.strftime("%d.%m.%Y-%H:%M:%S")
@@ -108,21 +109,22 @@ class Camera_widget(QWidget):
         self.count = 0
         self.capture_RTI()
 
-
+    #excecutes the RTI captures by setting the LEDs and capturing an image
     def capture_RTI(self):
         self.uiWindow.ui.progressBar.show()
         self.uiWindow.ui.progressBar.setValue(int((self.count / 24) * 100))
         time.sleep(0.1)
         self.arduino.serial(f'LED1{self.count}')
         time.sleep(0.1)
+        #capture an image
         cfg = self.camera_controls.picam2.create_still_configuration()
         targetPath=f"/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/RTI_SCAN_{self.timeStamp}/img_{self.count}.jpg"
         print("- Capture image:", targetPath)
         self.camera_controls.picam2.switch_mode_and_capture_file(cfg, targetPath, signal_function=self.qpicamera2.signal_done)
 
-
+    #performs a stereo photography capture by taking two pictures and moving the stage in between
     def stereo_photography(self, number):
-        if (number == 1):
+        if (number == 1):#make the directory first time this function is called
             self.timeStamp = time.strftime("%d.%m.%Y-%H:%M:%S")
             mkdir(f"/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/SP_SCAN_{self.timeStamp}")
 
@@ -132,12 +134,13 @@ class Camera_widget(QWidget):
 
         time.sleep(0.5*delta)
 
+        #capture an image
         cfg = self.camera_controls.picam2.create_still_configuration()
         targetPath=f"/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/SP_SCAN_{self.timeStamp}/img_{number}_{delta}mm.jpg"
         print("- Capture image:", targetPath)
         self.camera_controls.picam2.switch_mode_and_capture_file(cfg, targetPath, signal_function=self.qpicamera2.signal_done)
 
-
+    #performs a focus stacking scan
     def focus_stack(self):
         height = self.uiWindow.ui.inputBox_FS_h.value()
         n = self.uiWindow.ui.inputBox_FS_n.value()
@@ -146,12 +149,13 @@ class Camera_widget(QWidget):
         self.uiWindow.ui.progressBar.show()
         self.uiWindow.ui.progressBar.setValue(int((self.count / n) * 100))
 
-        if self.count == 0:
+        if self.count == 0:#make the directory first time this function is called
             #self.arduino.serial(f'Z0')
             self.timeStamp = time.strftime("%d.%m.%Y-%H:%M:%S")
             mkdir(f"/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/FS_SCAN_{self.timeStamp}")
             time.sleep(5)
 
+        #capture an image
         cfg = self.camera_controls.picam2.create_still_configuration()
         targetPath=f"/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/FS_SCAN_{self.timeStamp}/img_{self.count + 1}_{delta}mm.jpg"
         print("- Capture image:", targetPath)
@@ -167,38 +171,41 @@ class Camera_widget(QWidget):
             self.uiWindow.ui.progressBar.hide()
             self.uiWindow.ui.progressBar.setValue(0)
 
-
+    #used for debugging purposes
     def test_focus(self):
         print("test autofocus")
         self.auto_focus = True
         cfg = self.camera_controls.picam2.create_preview_configuration()
         img = self.camera_controls.picam2.switch_mode_and_capture_array(cfg, signal_function=self.qpicamera2.signal_done)
         
-
+    #performs the autofocus function
     def autofocus(self, img, n):
         self.uiWindow.ui.progressBar.show()
         self.uiWindow.ui.progressBar.setValue(int((self.count / n) * 100))
-
+        
         self.count += 1
         crop = 50
 
+        #exclude the edges of the image, as you want the center to be in focus
         img_center = img[(int((img.shape[0]/2)-crop)):(int((img.shape[0]/2)+crop)),
                          (int((img.shape[1]/2)-crop)):(int((img.shape[1]/2)+crop))]
 
-        #blur_value_alt = numpy.max(cv2.convertScaleAbs(cv2.Laplacian(img, 3)))
+        #use the laplacian oparator to get a value for the sharpness
+        #value depends on the sample but in general:
+        #0 - 3 is completely blurred, 3 - 8 is blurry, 9 + is in focus
         
         blur_value = round(numpy.std(cv2.Laplacian(img_center, cv2.CV_64F)), 2)
         self.arduino.serial(f"AF{blur_value}")
 
-        if blur_value > 8:
+        if blur_value > 8:#if it is getting sharp, go into fine control mode
             self.arduino.serial("AFF")
             
-        if self.count == n:
+        if self.count == n:#if the loop is done but the image is not in focus at all, restart
             if blur_value < 5:
                 time.sleep(0.1)
                 print("autofocus failed, restarting...")
 
-            else:
+            else:#else, done
                 time.sleep(0.1)
                 self.auto_focus = False
                 self.uiWindow.ui.progressBar.hide()
