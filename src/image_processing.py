c2cd9bae3d0eef22a93a909eb6d3cffae8e794e9
trellia003 <treccanielia03@gmail.com>
2025-10-17 11:41:44 +0200

    no need for these 2 files? commented out

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,14 +1,14 @@
-import numpy as np
-import cv2 as cv
-from matplotlib import pyplot as plt
-
-#this was an experiment to extract a depthmap from stereo photography scans
-#the result was very noisy but I included it in case anyone can figure out how to fix it (:
-
-imgR = cv.imread('/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/SP_test/L.jpg', cv.IMREAD_GRAYSCALE)
-imgL = cv.imread('/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/SP_test/R.jpg', cv.IMREAD_GRAYSCALE)
-
-stereo = cv.StereoBM_create(numDisparities=64, blockSize=5)
-disparity = stereo.compute(imgL,imgR)
-plt.imshow(disparity,'gray')
-plt.show()
+# import numpy as np
+# import cv2 as cv
+# from matplotlib import pyplot as plt
+#
+# #this was an experiment to extract a depthmap from stereo photography scans
+# #the result was very noisy but I included it in case anyone can figure out how to fix it (:
+#
+# imgR = cv.imread('/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/SP_test/L.jpg', cv.IMREAD_GRAYSCALE)
+# imgL = cv.imread('/home/xxpepxx/Desktop/GitProject/MicroscopyProject/src/captures/SP_test/R.jpg', cv.IMREAD_GRAYSCALE)
+#
+# stereo = cv.StereoBM_create(numDisparities=64, blockSize=5)
+# disparity = stereo.compute(imgL,imgR)
+# plt.imshow(disparity,'gray')
+# plt.show()
--- src/led_button_controller.py
+++ src/led_button_controller.py
@@ -1,15 +1,15 @@
-class LedButtonController:
-    def __init__(self, button):
-        self.button = button
-        self.is_yellow = False  # To keep track of the current color state
-        self.button.clicked.connect(self.toggle_color)  # Connect the button click to toggle_color method
-
-    
-    def toggle_color(self):
-        if self.is_yellow:
-            self.button.setStyleSheet("background-color: #919191")
-        else:
-            self.button.setStyleSheet("background-color: yellow")
-        self.is_yellow = not self.is_yellow  # Toggle the state
-
-    
+# class LedButtonController:
+#     def __init__(self, button):
+#         self.button = button
+#         self.is_yellow = False  # To keep track of the current color state
+#         self.button.clicked.connect(self.toggle_color)  # Connect the button click to toggle_color method
+#
+#
+#     def toggle_color(self):
+#         if self.is_yellow:
+#             self.button.setStyleSheet("background-color: #919191")
+#         else:
+#             self.button.setStyleSheet("background-color: yellow")
+#         self.is_yellow = not self.is_yellow  # Toggle the state
+#
+#
